834a5df70e5bd152f949dd3eecae52fa1b0c1279
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-09 11:45:50 +0000

    xxx

--- app.py
+++ app.py
@@ -17,12 +17,14 @@ st.markdown(
     [data-testid="column"] {{
     box-shadow: rgb(0 0 0 / 20%) 0px 2px 1px -1px, rgb(0 0 0 / 14%) 0px 1px 1px 0px, rgb(0 0 0 / 12%) 0px 1px 3px 0px;
     border-radius: 15px;
-    padding: 5% 5% 5% 10%;
+    
 }}
 
 
 [data-testid="stVerticalBlock"] > [style*="flex-direction: column;"] > [data-testid="stVerticalBlock"] {{
     box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    border-radius: 15px;
+
 }}
     
     .stApp {{
