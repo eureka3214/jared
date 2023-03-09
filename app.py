17f7908d38b40346d8ebf9b234a9f041e358b698
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-09 10:28:04 +0000

    xxx

--- app.py
+++ app.py
@@ -11,18 +11,7 @@ st.set_page_config(page_title="My App", page_icon=":rocket:", layout="wide",init
 st.markdown(
     f"""
     <style>
-    .stApp {{
-            background: rgba( 255, 255, 255, 0.65 );
-            box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
-            backdrop-filter: blur( 20px );
-            -webkit-backdrop-filter: blur( 20px );
-            border-radius: 10px;
-            border: 1px solid rgba( 255, 255, 255, 0.18 );
-            position: fixed;
-            height: 32em;
-        
-    }}
-
+   
     .sidebar .sidebar-content {{
                 background: rgba( 255, 255, 255, 0 );
                 width: 200px;
