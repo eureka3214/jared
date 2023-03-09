d9b0baea036520325d8fcc3dc661d5f466b9f49d
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-09 16:34:56 +0000

    ddd

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ st.markdown(
 
     .circular-container {{
   border-radius: 10%;
-  width: 900px;
-  height: 300px;
+  width: 500px;
+  height: 500px;
   display: flex;
   justify-content: center;
   align-items: center;
