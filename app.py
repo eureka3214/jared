3ecd451c153c7c72ed7d4795edc3fafe30b7e6c2
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-09 08:35:44 +0000

    nn

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.markdown(
     f"""
     <style>
     .stApp {{
-        background: rgba( 255, 255, 255, 0.4 );
+        background: rgba( 61, 183, 242, 0.4 );
         box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
         backdrop-filter: blur( 4px );
         -webkit-backdrop-filter: blur( 4px );
