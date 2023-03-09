9fd743629929ae85c0390b21ef6ef4899dadeb6c
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-09 16:37:14 +0000

    XXX

--- app.py
+++ app.py
@@ -110,10 +110,9 @@ if choose == "Write For Me":
 
         descript = st.text_area("Description",label_visibility='visible')
 
-        output = st.text_area("Heres your Text",label_visibility='visible')
         save = st.button("Save")
         if save:
-
+            output = st.text_area("Heres your Text",label_visibility='visible')
             query = str(usecase) + str(tone) + str(descript)
             st.write(query)
 
