8d97214a66b4588bdd1d1ded23b1fc5ccd058c46
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-09 12:56:10 +0000

    nn

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ st.markdown(
 
 
  [data-baseweb="select"] {{
-   box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+  box-shadow: rgba(0, 0, 0, 0.56) 0px 22px 70px 4px;
     border-radius: 15px;
     
     
