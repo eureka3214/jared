f599865d23b502f73156776a6e9a0868c45b86a3
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-09 16:32:42 +0000

    ddd

--- app.py
+++ app.py
@@ -7,25 +7,6 @@ import openai
 import os
 openai.api_key =  os.getenv("APIKEY")
 st.set_page_config(page_title="My App", page_icon=":rocket:", layout="wide",initial_sidebar_state="expanded"  )
-# st.write('<style>div.block-container{padding-top:0rem;}</style>', unsafe_allow_html=True)
-
-#    [data-testid="column"] {{
-#     box-shadow: rgb(0 0 0 / 20%) 0px 2px 1px -1px, rgb(0 0 0 / 14%) 0px 1px 1px 0px, rgb(0 0 0 / 12%) 0px 1px 3px 0px;
-#     border-radius: 15px;
-#     padding:6px;
-    
-# }}
-
-#   .stTextArea {{ 
-#             font-size:20px; 
-#             font-weight:bold; 
-
-#             }}
-
-    # .stApp {{
-       
-       
-    # }}
 
    
 
@@ -33,6 +14,13 @@ st.markdown(
     f"""
     <style>
     #MainMenu {{visibility: hidden;}}
+
+    .circular-container {{
+  border-radius: 10%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}}
 
     .appview-container .main .block-container{{
         padding: 2rem;
@@ -98,7 +86,10 @@ st.markdown(
     unsafe_allow_html=True
     )
 # st.markdown("<style> .stTextArea > label {font-size:14px; font-weight:bold;} </style> ",unsafe_allow_html=True) 
-   
+
+st.markdown('<div class="circular-container">', unsafe_allow_html=True)
+
+
 with st.sidebar:
     choose = option_menu("App Gallery", ["Write For Me", "Idea Generator", "Promotion Ideas", "Account", "Log Out"],
                          icons=['cpu', 'lightbulb fill', 'bar-chart fill', 'book','person dash'],
@@ -134,100 +125,4 @@ if choose == "Write For Me":
             st.write(query)
 
 
-        # columa, columb = st.columns([10,1])
-        # with columb:
-        
-        
-            
-
-
-
-
-
-
-# col1, col2,= st.columns(2)
-
-# if choose == "Write For Me":
-#     # with col1:
-#     st.subheader('Write for me')
-#     des=st.text_area(label='Description',label_visibility='collapsed' )
-#     if st.button('Submit'):
-#         reply = openai.Completion.create(
-#                                     engine="text-davinci-003",
-#                                     prompt=des,
-#                                     max_tokens=3600,
-#                                     n=1,
-#                                     stop=None,
-#                                     temperature=0.5,
-#                                     )
-#         explan= reply.choices[0].text.strip()
-#         # st.code(explan)
-#     # with col2:
-#     st.subheader('Generated OutPuts')
-
-#     try:
-#         explan= reply.choices[0].text.strip()
-#         st.code(explan)
-#     except:
-#         st.code("Input something and click submit")
-#         # st.code(explan)
-#             # st.stop()
-                
-# elif choose == "Idea Generator":
-#     with col1:
-
-#         st.subheader('Idea Generator')
-#         src = st.text_input("source Link")
-#         des=st.text_area(label='Description',label_visibility='collapsed' )
-#         if st.button('Submit'):
-#             prompt = str(des) + " " + " Response should be based from this source :" + str(src)
-#             reply = openai.Completion.create(
-#                                         engine="text-davinci-003",
-#                                         prompt=des,
-#                                         max_tokens=3600,
-#                                         n=1,
-#                                         stop=None,
-#                                         temperature=0.5,
-#                                         )
-#             idea= reply.choices[0].text.strip()
-#             # st.code(explan)
-#     with col2:
-#         st.subheader('Generated OutPuts')
-
-#         try:
-#             idea= reply.choices[0].text.strip()
-#             st.code(idea)
-#         except:
-#             st.code("Input something and click submit")
-#         # st.code(explan)
-# # st.stop()
-
-
-# elif choose == "Promotion Ideas":
-#     with col1:
-#         st.subheader('Promotion Ideas')
-#         string_list = ['OnlyFans mass Message', 'Caption For Instagram Post', 'Reply for a Tweet or Message']
-#         selected_option = st.selectbox('Select an option', string_list, label_visibility ='collapsed')
-# # Define the options for the select box
-#         options = [' That Would arrouse and turn on the guy who is reading it and make him ask for nudes ', ' that Would grab the attention of the person who is reads it and well suits the caption ', ' a sarcastic response']
-#         des=st.text_area(label='Description',label_visibility='collapsed' ,placeholder='Specify under which context')
-#         promp = "Generate a " + selected_option + ' ' + options[string_list.index(selected_option)] + ' Under the following context' + des
-#         if st.button('Submit'):
-#             reply = openai.Completion.create(
-#                                         engine="text-davinci-003",
-#                                         prompt=str(promp),
-#                                         max_tokens=3600,
-#                                         n=1,
-#                                         stop=None,
-#                                         temperature=0.5,
-#                                         )
-#             explan= reply.choices[0].text.strip()
-#             # st.code(explan)
-#     with col2:
-#         st.subheader('Generated OutPuts')
-
-#         try:
-#             explan= reply.choices[0].text.strip()
-#             st.code(explan)
-#         except:
-#             st.code("Input something and click submit")
+st.markdown('</div>', unsafe_allow_html=True)
