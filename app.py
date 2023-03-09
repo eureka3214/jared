8ae0619f288214d58d41fb45e790a869822d9df8
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-09 14:19:08 +0000

    mmm

--- app.py
+++ app.py
@@ -34,6 +34,8 @@ st.markdown(
     <style>
     #MainMenu {{visibility: hidden;}}
 
+    div.stButton > button:first-child {{ border: 5px solid purple; border-radius:20px 20px 20px 20px; }}
+
     sidebar .sidebar-content {{
     background-color: #111 !important;
     box-shadow: rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px;
