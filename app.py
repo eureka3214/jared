52060a0e55066046ab808073a24f2ad0b738075a
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-09 12:51:54 +0000

    sss

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ st.markdown(
     #MainMenu {{visibility: hidden;}}
 
  [data-baseweb="textarea"] {{
-    box-shadow: box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;
+   box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
     border-radius: 15px;
     padding:6px;
     width:96%;
