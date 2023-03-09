4dce51b097f1113f25393473fea5f719cac2759b
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-09 08:50:28 +0000

    ddd

--- app.py
+++ app.py
@@ -49,118 +49,104 @@ with st.sidebar:
     }
     )
 
-    
-# def openai(description):
-#     reply = openai.Completion.create(
+
+
+if choose == "Write For Me":
+
+    col1, col2,= st.columns(2)
+    with col1:
+        usecase = st.selectbox("Use Case",  ('Email', 'Home phone', 'Mobile phone'),label_visibility='visible' )
+    with col2:
+        tone = st.selectbox("Select Tone",  ('Email', 'Home phone', 'Mobile phone'),label_visibility='visible')
+
+
+
+
+
+
+# col1, col2,= st.columns(2)
+
+# if choose == "Write For Me":
+#     # with col1:
+#     st.subheader('Write for me')
+#     des=st.text_area(label='Description',label_visibility='collapsed' )
+#     if st.button('Submit'):
+#         reply = openai.Completion.create(
+#                                     engine="text-davinci-003",
+#                                     prompt=des,
+#                                     max_tokens=3600,
+#                                     n=1,
+#                                     stop=None,
+#                                     temperature=0.5,
+#                                     )
+#         explan= reply.choices[0].text.strip()
+#         # st.code(explan)
+#     # with col2:
+#     st.subheader('Generated OutPuts')
+
+#     try:
+#         explan= reply.choices[0].text.strip()
+#         st.code(explan)
+#     except:
+#         st.code("Input something and click submit")
+#         # st.code(explan)
+#             # st.stop()
+                
+# elif choose == "Idea Generator":
+#     with col1:
+
+#         st.subheader('Idea Generator')
+#         src = st.text_input("source Link")
+#         des=st.text_area(label='Description',label_visibility='collapsed' )
+#         if st.button('Submit'):
+#             prompt = str(des) + " " + " Response should be based from this source :" + str(src)
+#             reply = openai.Completion.create(
 #                                         engine="text-davinci-003",
-#                                         prompt=inpt,
+#                                         prompt=des,
 #                                         max_tokens=3600,
 #                                         n=1,
 #                                         stop=None,
 #                                         temperature=0.5,
 #                                         )
-#     explan= reply.choices[0].text.strip()
-#     st.stop()
-#     return explan
+#             idea= reply.choices[0].text.strip()
+#             # st.code(explan)
+#     with col2:
+#         st.subheader('Generated OutPuts')
 
- 
-    
-st.markdown(""" <style> 
-    .form {
-   
-background: rgba(255, 255, 255, 0.2);
-border-radius: 16px;
-box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-backdrop-filter: blur(5px);
--webkit-backdrop-filter: blur(5px);
-border: 1px solid rgba(255, 255, 255, 0.3);
-
-     } 
-    </style> """, unsafe_allow_html=True)
-
-col1, col2,= st.columns(2)
-
-if choose == "Write For Me":
-    with col1:
-        st.subheader('Write for me')
-        des=st.text_area(label='Description',label_visibility='collapsed' )
-        if st.button('Submit'):
-            reply = openai.Completion.create(
-                                        engine="text-davinci-003",
-                                        prompt=des,
-                                        max_tokens=3600,
-                                        n=1,
-                                        stop=None,
-                                        temperature=0.5,
-                                        )
-            explan= reply.choices[0].text.strip()
-            # st.code(explan)
-    with col2:
-        st.subheader('Generated OutPuts')
-
-        try:
-            explan= reply.choices[0].text.strip()
-            st.code(explan)
-        except:
-            st.code("Input something and click submit")
-        # st.code(explan)
-            # st.stop()
-                
-elif choose == "Idea Generator":
-    with col1:
-
-        st.subheader('Idea Generator')
-        src = st.text_input("source Link")
-        des=st.text_area(label='Description',label_visibility='collapsed' )
-        if st.button('Submit'):
-            prompt = str(des) + " " + " Response should be based from this source :" + str(src)
-            reply = openai.Completion.create(
-                                        engine="text-davinci-003",
-                                        prompt=des,
-                                        max_tokens=3600,
-                                        n=1,
-                                        stop=None,
-                                        temperature=0.5,
-                                        )
-            idea= reply.choices[0].text.strip()
-            # st.code(explan)
-    with col2:
-        st.subheader('Generated OutPuts')
-
-        try:
-            idea= reply.choices[0].text.strip()
-            st.code(idea)
-        except:
-            st.code("Input something and click submit")
-        # st.code(explan)
-# st.stop()
+#         try:
+#             idea= reply.choices[0].text.strip()
+#             st.code(idea)
+#         except:
+#             st.code("Input something and click submit")
+#         # st.code(explan)
+# # st.stop()
 
 
-elif choose == "Promotion Ideas":
-    with col1:
-        st.subheader('Promotion Ideas')
-        string_list = ['OnlyFans mass Message', 'Caption For Instagram Post', 'Reply for a Tweet or Message']
-        selected_option = st.selectbox('Select an option', string_list, label_visibility ='collapsed')
-# Define the options for the select box
-        options = [' That Would arrouse and turn on the guy who is reading it and make him ask for nudes ', ' that Would grab the attention of the person who is reads it and well suits the caption ', ' a sarcastic response']
-        des=st.text_area(label='Description',label_visibility='collapsed' ,placeholder='Specify under which context')
-        promp = "Generate a " + selected_option + ' ' + options[string_list.index(selected_option)] + ' Under the following context' + des
-        if st.button('Submit'):
-            reply = openai.Completion.create(
-                                        engine="text-davinci-003",
-                                        prompt=str(promp),
-                                        max_tokens=3600,
-                                        n=1,
-                                        stop=None,
-                                        temperature=0.5,
-                                        )
-            explan= reply.choices[0].text.strip()
-            # st.code(explan)
-    with col2:
-        st.subheader('Generated OutPuts')
+# elif choose == "Promotion Ideas":
+#     with col1:
+#         st.subheader('Promotion Ideas')
+#         string_list = ['OnlyFans mass Message', 'Caption For Instagram Post', 'Reply for a Tweet or Message']
+#         selected_option = st.selectbox('Select an option', string_list, label_visibility ='collapsed')
+# # Define the options for the select box
+#         options = [' That Would arrouse and turn on the guy who is reading it and make him ask for nudes ', ' that Would grab the attention of the person who is reads it and well suits the caption ', ' a sarcastic response']
+#         des=st.text_area(label='Description',label_visibility='collapsed' ,placeholder='Specify under which context')
+#         promp = "Generate a " + selected_option + ' ' + options[string_list.index(selected_option)] + ' Under the following context' + des
+#         if st.button('Submit'):
+#             reply = openai.Completion.create(
+#                                         engine="text-davinci-003",
+#                                         prompt=str(promp),
+#                                         max_tokens=3600,
+#                                         n=1,
+#                                         stop=None,
+#                                         temperature=0.5,
+#                                         )
+#             explan= reply.choices[0].text.strip()
+#             # st.code(explan)
+#     with col2:
+#         st.subheader('Generated OutPuts')
 
-        try:
-            explan= reply.choices[0].text.strip()
-            st.code(explan)
-        except:
-            st.code("Input something and click submit")
+#         try:
+#             explan= reply.choices[0].text.strip()
+#             st.code(explan)
+#         except:
+#             st.code("Input something and click submit")
