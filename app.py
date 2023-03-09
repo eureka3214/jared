0298130f9edf8b1f6cc8b840db2c7b721631124e
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-09 11:43:33 +0000

    mm

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.markdown(
     box-shadow: rgb(0 0 0 / 20%) 0px 2px 1px -1px, rgb(0 0 0 / 14%) 0px 1px 1px 0px, rgb(0 0 0 / 12%) 0px 1px 3px 0px;
     border-radius: 15px;
     padding: 5% 5% 5% 10%;
-} }
+}}
 
 
 [data-testid="stVerticalBlock"] > [style*="flex-direction: column;"] > [data-testid="stVerticalBlock"] {{
